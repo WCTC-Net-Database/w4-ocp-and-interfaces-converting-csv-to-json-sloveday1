177db17bf89b9a6f45a5f09c4d518d4d73836659
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-20 23:37:21 -0500

    Update JsonFileHandler.cs

--- Services/JsonFileHandler.cs
+++ Services/JsonFileHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using W4_assignment_template.Interfaces;
 using W4_assignment_template.Models;
 
@@ -8,12 +9,17 @@ public class JsonFileHandler : IFileHandler
     public List<Character> ReadCharacters(string filePath)
     {
         // TODO: Implement JSON reading logic
-        throw new NotImplementedException();
+        
+        var json = File.ReadAllText(filePath);
+        var characters = JsonConvert.DeserializeObject<List<Character>>(json);
+
+        return characters;
     }
 
     public void WriteCharacters(string filePath, List<Character> characters)
     {
         // TODO: Implement JSON writing logic
-        throw new NotImplementedException();
+        string json = JsonConvert.SerializeObject(characters);
+        File.WriteAllText(filePath, json);
     }
 }
