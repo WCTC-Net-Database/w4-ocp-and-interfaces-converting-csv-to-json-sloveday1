8e26a6b4c8136b1e919477d0d845d8fe8d4841f8
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-20 23:36:42 -0500

    Update CsvFileHandler.cs

--- Services/CsvFileHandler.cs
+++ Services/CsvFileHandler.cs
@@ -1,19 +1,65 @@
-using W4_assignment_template.Interfaces;
-using W4_assignment_template.Models;
-
 namespace W4_assignment_template.Services;
 
 public class CsvFileHandler : IFileHandler
 {
+    
     public List<Character> ReadCharacters(string filePath)
     {
         // TODO: Implement CSV reading logic
-        throw new NotImplementedException();
+        List<Character> characters = new List<Character>();
+        string[] lines = File.ReadAllLines(filePath);
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i];
+
+            string name;
+            int commaIndex = line.IndexOf(",");
+
+            if (line.StartsWith("\""))
+            {
+                var firstQuote = line.IndexOf("\"");
+                line = line.Substring(firstQuote + 1);
+                var lastQuote = line.IndexOf("\"");
+                commaIndex = line.IndexOf(",", lastQuote);
+                name = line.Substring(0, commaIndex - 1);
+            }
+            else
+            {
+                name = line.Substring(0, commaIndex);
+            }
+
+            string[] fields = line.Split(',');
+            string characterClass = fields[fields.Length - 4];
+            int level = Convert.ToInt32(fields[fields.Length - 3]);
+            int hitPoints = Convert.ToInt32(fields[fields.Length - 2]);
+
+            List<string> equipment = new List<string>(fields[fields.Length - 1].Split("|"));
+
+            Character character = new Character()
+                { Name = name, Class = characterClass, Level = level, HP = hitPoints, Equipment = equipment };
+            
+            characters.Add(character);
+        }
+
+        return characters;
     }
 
     public void WriteCharacters(string filePath, List<Character> characters)
     {
         // TODO: Implement CSV writing logic
-        throw new NotImplementedException();
+        string[] lines = new string[characters.Count];
+
+        foreach (var character in characters)
+        {
+            for (int i = 1; i < characters.Count; i++)
+            {
+                string line =
+                    $"{character.Name},{character.Class},{character.Level},{character.HP},{character.Equipment}";
+                lines[i] = line;
+            }
+        }
+
+        File.WriteAllLines(filePath, lines);
     }
 }
